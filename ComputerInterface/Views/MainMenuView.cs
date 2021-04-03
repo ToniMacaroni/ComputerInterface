0b329e88793ad04f383731d8a1209d1e6c898214
Toni Macaroni <tonimacaroniy@gmail.com>
2021-04-03 03:38:34 +0200

    don't show entry if mod is disabled

--- ComputerInterface/Views/MainMenuView.cs
+++ ComputerInterface/Views/MainMenuView.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using BepInEx.Bootstrap;
 using ComputerInterface.Interfaces;
 using ComputerInterface.ViewLib;
 
@@ -8,6 +10,8 @@ namespace ComputerInterface.Views
     public class MainMenuView : ComputerView
     {
         private List<IComputerModEntry> _modEntries;
+        private readonly List<IComputerModEntry> _shownEntries;
+        private readonly Dictionary<IComputerModEntry, BepInEx.PluginInfo> _pluginInfoMap;
 
         private readonly UIElementPageHandler<IComputerModEntry> _pageHandler;
         private readonly UISelectionHandler _selectionHandler;
@@ -24,16 +28,43 @@ namespace ComputerInterface.Views
             _pageHandler.NextMark = "▼";
             _pageHandler.PrevMark = "▲";
             _pageHandler.EntriesPerPage = 8;
+
+            _shownEntries = new List<IComputerModEntry>();
+            _pluginInfoMap = new Dictionary<IComputerModEntry, BepInEx.PluginInfo>();
         }
 
         public void ShowEntries(List<IComputerModEntry> entries)
         {
             _modEntries = entries;
-            _selectionHandler.MaxIdx = _modEntries.Count - 1;
 
-            _pageHandler.SetElements(entries.ToArray());
+            // Map entries to plugin infos
+            _pluginInfoMap.Clear();
+            foreach (var entry in entries)
+            {
+                var asm = entry.GetType().Assembly;
+                var pluginInfo =
+                    Chainloader.PluginInfos.Values.FirstOrDefault(x => x.Instance.GetType().Assembly == asm);
+                if (pluginInfo != null)
+                {
+                    _pluginInfoMap.Add(entry, pluginInfo);
+                }
+            }
+
+            FilterEntries();
 
             Redraw();
+        }
+
+        public void FilterEntries()
+        {
+            _shownEntries.Clear();
+            foreach (var entry in _modEntries)
+            {
+                if (!_pluginInfoMap.TryGetValue(entry, out var info)) continue;
+                if(info.Instance.enabled) _shownEntries.Add(entry);
+            }
+            _selectionHandler.MaxIdx = _shownEntries.Count - 1;
+            _pageHandler.SetElements(_shownEntries.ToArray());
         }
 
         public void Redraw()
@@ -76,6 +107,9 @@ namespace ComputerInterface.Views
         public override void OnShow(object[] args)
         {
             base.OnShow(args);
+            if (_modEntries == null) return;
+            FilterEntries();
+            Redraw();
         }
 
         public override void OnKeyPressed(EKeyboardKey key)
