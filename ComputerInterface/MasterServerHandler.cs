285f2368e0327561309ca95015035c4a9475419c
RusJJ <rusjj1337@gmail.com>
2021-08-04 20:05:54 +0300

    Fix Sound Post with a music

--- ComputerInterface/MasterServerHandler.cs
+++ ComputerInterface/MasterServerHandler.cs
@@ -7,15 +7,12 @@ using UnityEngine;
 
 namespace ComputerInterface
 {
-    public class MOTDHandler : MonoBehaviourPunCallbacks
+    public class MasterServerHandler : MonoBehaviourPunCallbacks
     {
         void Awake()
-        {
-            Debug.Log("Computer Interface: MOTDHandler::Awake");
-        }
+        {}
         private static void OnReturnCurrentVersion(ExecuteCloudScriptResult result)
         {
-            Debug.Log("Computer Interface got MOTD answer!");
             JsonObject functionResult = (JsonObject)result.FunctionResult;
             object obj;
             GorillaComputer.instance.motdText.text = !functionResult.TryGetValue("MOTD", out obj) || !PhotonNetworkController.instance.gameVersion.Contains("live") ? (!functionResult.TryGetValue("MOTDBeta", out obj) || !PhotonNetworkController.instance.gameVersion.Contains("beta") ? "UNABLE TO LOAD MOTD" : (string)obj) : (string)obj;
@@ -24,13 +21,22 @@ namespace ComputerInterface
         {
             Debug.LogError("Computer Interface failed at MOTD loading!");
         }
+        private void OnGetTimeSuccess(GetTimeResult result)
+        {
+            GorillaComputer.instance.startupMillis = result.Time.Ticks / 10000L - (long)(Time.realtimeSinceStartup * 1000.0);
+        }
+        private void OnGetTimeFailure(PlayFabError error)
+        {
+            Debug.LogError("There was a problem getting the time. Error: " + error.GenerateErrorReport() + ". Trying again.");
+            PlayFabClientAPI.GetTime(new GetTimeRequest(), new Action<GetTimeResult>(OnGetTimeSuccess), new Action<PlayFabError>(OnGetTimeFailure));
+        }
         public override void OnConnectedToMaster()
         {
-            Debug.Log("Computer Interface trying to catch MOTD...");
             PlayFabClientAPI.ExecuteCloudScript(new ExecuteCloudScriptRequest()
             {
                 FunctionName = "ReturnCurrentVersion"
             }, new Action<ExecuteCloudScriptResult>(OnReturnCurrentVersion), new Action<PlayFabError>(OnErrorShared));
+            PlayFabClientAPI.GetTime(new GetTimeRequest(), new Action<GetTimeResult>(OnGetTimeSuccess), new Action<PlayFabError>(OnGetTimeFailure));
         }
     }
 }
