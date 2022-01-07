4cbfd4916b1e4af2db3ee4e3de2c249d6728d149
Graicc <33105645+Graicc@users.noreply.github.com>
2022-01-06 23:26:08 -0500

    Improve mod list view

--- ComputerInterface/Views/ModListView.cs
+++ ComputerInterface/Views/ModListView.cs
@@ -4,6 +4,7 @@ using System.Text;
 using BepInEx.Bootstrap;
 using ComputerInterface.Interfaces;
 using ComputerInterface.ViewLib;
+using HarmonyLib;
 
 namespace ComputerInterface.Views
 {
@@ -15,21 +16,71 @@ namespace ComputerInterface.Views
 
     internal class ModListView : ComputerView
     {
-        private readonly BepInEx.PluginInfo[] _plugins;
+		internal class ModListItem
+		{
+            private readonly CIConfig _config;
 
-        private readonly UIElementPageHandler<BepInEx.PluginInfo> _pageHandler;
+			public BepInEx.PluginInfo PluginInfo { get; private set; }
+			public bool Supported { get; private set; }
+
+            public ModListItem(BepInEx.PluginInfo pluginInfo, CIConfig config)
+			{
+                _config = config;
+                PluginInfo = pluginInfo;
+                Supported = DoesModImplementFeature();
+			}
+
+			private bool DoesModImplementFeature()
+			{
+                var onEnable = AccessTools.Method(PluginInfo.Instance.GetType(), "OnEnable");
+                var onDisable = AccessTools.Method(PluginInfo.Instance.GetType(), "OnDisable");
+                return onEnable != null && onDisable != null;
+			}
+
+            public void EnableMod()
+			{
+                PluginInfo.Instance.enabled = true;
+                _config.RemoveDisabledMod(PluginInfo.Metadata.GUID);
+			}
+
+            public void DisableMod()
+			{
+                PluginInfo.Instance.enabled = false;
+                _config.AddDisabledMod(PluginInfo.Metadata.GUID);
+			}
+
+            public void ToggleMod()
+			{
+                if (PluginInfo.Instance.enabled)
+				{
+                    DisableMod();
+				} else
+				{
+                    EnableMod();
+				}
+			}
+		}
+
+        private readonly CIConfig _config;
+
+        private readonly ModListItem[] _plugins;
+
+        private readonly UIElementPageHandler<ModListItem> _pageHandler;
         private readonly UISelectionHandler _selectionHandler;
 
-        public ModListView()
+        public ModListView(CIConfig config)
         {
-            _plugins = Chainloader.PluginInfos.Values.ToArray();
+            _config = config;
+
+            var pluginInfos = Chainloader.PluginInfos.Values.Where(plugin => !plugin.Metadata.GUID.Contains(PluginInfo.Id));
+            _plugins = pluginInfos.Select(plugin => new ModListItem(plugin, _config)).OrderBy(x => !x.Supported).ToArray();
             _selectionHandler =
                 new UISelectionHandler(EKeyboardKey.Up, EKeyboardKey.Down, EKeyboardKey.Enter);
             _selectionHandler.MaxIdx = _plugins.Length - 1;
             _selectionHandler.OnSelected += SelectMod;
             _selectionHandler.ConfigureSelectionIndicator("<color=#ed6540>> </color>", "", "  ", "");
 
-            _pageHandler = new UIElementPageHandler<BepInEx.PluginInfo>();
+            _pageHandler = new UIElementPageHandler<ModListItem>();
             _pageHandler.EntriesPerPage = 10;
 
             _pageHandler.SetElements(_plugins);
@@ -60,15 +111,22 @@ namespace ComputerInterface.Views
 
         private void DrawMods(StringBuilder str)
         {
-            var enabledPostfix = "<color=#00ff00>  E</color>";
-            var disabledPostfix = "<color=#ff0000>  D</color>";
+            // var enabledPostfix = "<color=#00ff00>  E</color>";
+            // var disabledPostfix = "<color=#ff0000>  D</color>";
+            var enabledPrefix = "<color=#00ff00> + </color>";
+            var disabledPrefix = "<color=#ff0000> - </color>";
+
+            var unsupportedColor = "ffffff50";
 
             var lineIdx = _pageHandler.MovePageToIdx(_selectionHandler.CurrentSelectionIndex);
 
             _pageHandler.EnumarateElements((plugin, idx) =>
             {
-                str.Append(_selectionHandler.GetIndicatedText(idx, lineIdx, plugin.Metadata.Name));
-                str.Append(plugin.Instance.enabled ? enabledPostfix : disabledPostfix);
+                str.Append(plugin.PluginInfo.Instance.enabled ? enabledPrefix : disabledPrefix);
+                if (!plugin.Supported) str.BeginColor(unsupportedColor);
+                str.Append(_selectionHandler.GetIndicatedText(idx, lineIdx, plugin.PluginInfo.Metadata.Name));
+                if (!plugin.Supported) str.EndColor();
+                // str.Append(plugin.Instance.enabled ? enabledPostfix : disabledPostfix);
                 str.AppendLine();
             });
 
@@ -88,6 +146,9 @@ namespace ComputerInterface.Views
 
             switch (key)
             {
+                case EKeyboardKey.Option1:
+                    ShowView<ModView>(_plugins[_selectionHandler.CurrentSelectionIndex]);
+                    break;
                 case EKeyboardKey.Back:
                     ReturnToMainMenu();
                     break;
@@ -96,7 +157,11 @@ namespace ComputerInterface.Views
 
         private void SelectMod(int idx)
         {
-            ShowView<ModView>(_plugins[idx]);
+            if (_plugins[idx].Supported)
+			{
+                _plugins[idx].ToggleMod();
+			}
+            Redraw();
         }
     }
 }
