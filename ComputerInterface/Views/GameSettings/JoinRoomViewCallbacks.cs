316e21be2b4df1a2276d2ddfa73ce28562b16d4d
fchb1239 <fchb1239.dk@gmail.com>
2022-08-06 19:45:15 +0200

    apperently i can't be lazy anymore (big F)

--- ComputerInterface/Views/GameSettings/JoinRoomViewCallbacks.cs
+++ ComputerInterface/Views/GameSettings/JoinRoomViewCallbacks.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 namespace ComputerInterface.Views.GameSettings
 {
-    class JoinRoomViewCallbacks : MonoBehaviour
+    class JoinRoomViewCallbacks : MonoBehaviourPunCallbacks
     {
         public JoinRoomView view;
 
-        // Yeah I'm pretty lazy
-        void Update()
-        {
-            view.Redraw();
-        }
+        public override void OnJoinedRoom() => view.Redraw();
+        public override void OnCreatedRoom() => view.Redraw();
+        public override void OnLeftRoom() => view.Redraw();
+        public override void OnDisconnected(DisconnectCause cause) => view.Redraw();
+        public override void OnJoinRoomFailed(short returnCode, string message) => view.Redraw();
+        public override void OnJoinRandomFailed(short returnCode, string message) => view.Redraw();
+        public override void OnCreateRoomFailed(short returnCode, string message) => view.Redraw();
+        // public override void OnSchoolFailed(short returnCode, string message) => view.Redraw();
+        public override void OnConnected() => view.Redraw();
+        public override void OnConnectedToMaster() => view.Redraw();
     }
 }
