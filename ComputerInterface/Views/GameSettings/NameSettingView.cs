472bf1e515553e2b93d0ffeaa64eba36f0702381
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-10-28 18:08:27 -0230

    Update ComputerInterface/Views/GameSettings/NameSettingView.cs
    Co-authored-by: Graic <33105645+Graicc@users.noreply.github.com>

--- ComputerInterface/Views/GameSettings/NameSettingView.cs
+++ ComputerInterface/Views/GameSettings/NameSettingView.cs
@@ -32,13 +32,6 @@ namespace ComputerInterface.Views.GameSettings
             str.AppendLine();
             str.BeginColor("ffffff50").Append("> ").EndColor().Append(_textInputHandler.Text).AppendClr("_", "ffffff50");
 
-            /* dunno if you would want this
-            if (!_textInputHandler.Text.IsNullOrWhiteSpace() && !GorillaNetworking.GorillaComputer.instance.CheckAutoBanListForName(_textInputHandler.Text))
-            {
-                str.BeginColor("7F0000").AppendLines(2);
-                str.Append("* This name was blacklisted by the name filter.").EndColor();
-            }
-
             if (!_textInputHandler.Text.IsNullOrWhiteSpace() && _textInputHandler.Text.Length > 12)
             {
                 str.BeginColor("7F0000").AppendLines(2);
